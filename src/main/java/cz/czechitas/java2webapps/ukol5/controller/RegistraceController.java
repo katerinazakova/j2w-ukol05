12739aac862cd4868a842d13bc186243720c56c5
7ZKaty <zakova.kz@gmail.com>
2023-10-28 22:54:00 +0200

    feat(RegistraceController): create controller method that will receive data from RegistraceForm

--- src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
@@ -1,17 +1,22 @@
 package cz.czechitas.java2webapps.ukol5.controller;
 
+import cz.czechitas.java2webapps.ukol5.entity.Gender;
+import cz.czechitas.java2webapps.ukol5.entity.RegistraceForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Kontroler obsluhující registraci účastníků dětského tábora.
  */
 @Controller
 public class RegistraceController {
-
-  @GetMapping("/")
-  public String formular() {
-    return "formular";
-  }
+    @GetMapping("/")
+    public ModelAndView formular() {
+        ModelAndView modelAndView = new ModelAndView("formular");
+        modelAndView.addObject("participant", new RegistraceForm());
+        modelAndView.addObject("gender", Gender.values());
+        return modelAndView;
+    }
 
 }
